2879dbeee3a970f7b4f9150b55769e8b42345c1f
dance2die <dontbothermewithspam@gmail.com>
2015-05-05 19:45:35 -0400

    First Async Await test

---
+++ Demo.Languages/Demo.Languages.Csharp.AsyncAwait.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace Demo.Languages.Csharp.AsyncAwait.Console
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string uri = "http://www.google.com";
+			var textTask = GetTextTask(uri);
+			System.Console.WriteLine(textTask.Result);
+		}
+
+		private async static Task<string> GetTextTask(string uri)
+		{
+			var client = new WebClient();
+			string data = await client.DownloadStringTaskAsync(uri);
+			System.Console.WriteLine("---------------------------------------------------");
+			return data;
+		}
+	}
+}
