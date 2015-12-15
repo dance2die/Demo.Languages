c3d16f4036dbdbc92110daebc68d4b676edcecc5
dance2die <dontbothermewithspam@gmail.com>
2015-12-15 12:06:42 -0500

    I rather not use TypeScript for now with AngularJS 1

---
+++ Demo.Languages/Demo.Languages.TypeScript/Scripts/TypeScripts/homeIndex.js
@@ -0,0 +1,9 @@
+/// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angularjs/angular-route.d.ts" />
+(function () {
+    "use strict";
+    var angular;
+    angular.module("homeIndexApp", ["ngRoute"])
+        .controller("homeIndexCtrl", function ($scope, $http) {
+    });
+})();
--- Demo.Languages/Demo.Languages.TypeScript/Scripts/homeIndexApp.js
+++ Demo.Languages/Demo.Languages.TypeScript/Scripts/homeIndexApp.js
@@ -1,3 +1,3 @@
-﻿angular.module("homeIndexApp", [])
-	.controller("homeIndexCtrl", function($scope) {
-	});
+﻿//angular.module("homeIndexApp", [])
+//	.controller("homeIndexCtrl", function($scope) {
+//	});
