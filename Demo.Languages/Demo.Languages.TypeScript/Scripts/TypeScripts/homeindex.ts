0ffed578588d59f8ac0de8d6ba956a9f811ef66c
dance2die <dontbothermewithspam@gmail.com>
2015-12-19 12:58:20 -0500

    vs 2015 github bug - homeindex.ts was not added

---
+++ Demo.Languages/Demo.Languages.TypeScript/Scripts/TypeScripts/homeindex.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../typings/angularjs/angular.d.ts" />
+/// <reference path="../typings/angularjs/angular-route.d.ts" />
+(function () {
+    "use strict";
+    //var angular: any;
+    var app = angular.module("homeIndexApp", ["ngRoute"]);
+    app.controller("homeIndexCtrl", function ($scope, $http) {
+        alert("homeIndexCtrl");
+    });
+})();
+//# sourceMappingURL=homeIndex.js.map
