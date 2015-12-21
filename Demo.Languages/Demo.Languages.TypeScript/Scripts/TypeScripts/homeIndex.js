df383e3e910fd9f50c6520c94e0ee3b8274138ae
dance2die <dontbothermewithspam@gmail.com>
2015-12-21 09:26:55 -0500

    example of using classes in Pro TypeScript

--- Demo.Languages/Demo.Languages.TypeScript/Scripts/TypeScripts/homeIndex.js
+++ Demo.Languages/Demo.Languages.TypeScript/Scripts/TypeScripts/homeIndex.js
@@ -6,9 +6,42 @@
     var app = angular.module("homeIndexApp", ["ngRoute"]);
     app.controller("homeIndexCtrl", function ($scope, $http) {
         $scope.testAlert = function () {
-            alert("test alert in AngularJS within TypeScript file");
+            var songs = [
+                new Song('Bushbaby', 'Megaphone'),
+                new Song('Delays', 'One More Lie In'),
+                new Song('Goober Gun', 'Stereo'),
+                new Song('Sohnee', 'Shatter'),
+                new Song('Get Amped', 'Celebrity')
+            ];
+            var jukebox = new Jukebox(songs);
+            jukebox.play();
         };
     });
+    var Song = (function () {
+        function Song(artist, title) {
+            this.artist = artist;
+            this.title = title;
+        }
+        Song.prototype.play = function () {
+            console.log("Playing " + this.title + ' by ' + this.artist);
+        };
+        return Song;
+    })();
+    var Jukebox = (function () {
+        function Jukebox(songs) {
+            this.songs = songs;
+        }
+        Jukebox.prototype.play = function () {
+            var song = this.getRandomSong();
+            song.play();
+        };
+        Jukebox.prototype.getRandomSong = function () {
+            var songCount = this.songs.length;
+            var songIndex = Math.floor(Math.random() * songCount);
+            return this.songs[songIndex];
+        };
+        return Jukebox;
+    })();
 })();
 //# sourceMappingURL=homeIndex.js.map 
 //# sourceMappingURL=homeindex.js.map
